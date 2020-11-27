41f95902f17aae7c9b263eae733edf119db744c2
SophieParent <soffee.parent@gmail.com>
2020-11-27 18:02:36 -0500

    Ajout des tests pour : couverture des arcs du graphe de flot de contrôle

--- Test/src/test/MainWindowTest.java
+++ Test/src/test/MainWindowTest.java
@@ -19,5 +19,13 @@ class MainWindowTest {
     void t1() {
         assertEquals(93.0, MainWindow.convert("US Dollar", "Euro", currencies, 100.0));
     }
+    
+	//Test partie 2 : T3 (couverture des arcs du graphe de flot de contrôle)
+    @Test
+    void t3() {
+    	assertEquals(93.0, MainWindow.convert("US Dollar", "Euro", currencies, 100.0));		// valide-valide
+        assertEquals(0.0, MainWindow.convert("Invalide", "Invalide", currencies, 100.0));	// invalide-invalide
+        assertEquals(0.0, MainWindow.convert("Invalide", "Euro", currencies, 100.0));		// invalide-valide
+    }
 
 }
